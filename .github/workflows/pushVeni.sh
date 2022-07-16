95fc2c1cab83528f5cee0748220ef7013e9303a6
Kana Ki <andrewj@ksharp.net>
2022-07-16 18:27:35 +0100

    Delete pushVeni.sh

--- .github/workflows/pushVeni.sh
+++
@@ -1,6 +0,0 @@
-cd FFXIVVenues.VeniKi
-docker build ./ -t ocridion/ffxivvenues.veni:latest --build-arg NUGET_REPO_PASSWORD=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
-docker push ocridion/ffxivvenues.veni:latest
-
-
-# docker run ocridion/ffxivvenues.veni:latest --env-file ./veni.env
