ecc4146097a44bb1bde119edb9feb33de4035761
Andy James <andrewj@ksharp.net>
2022-08-14 04:23:05 +0100

    Fix id for Sumi

--- Utils/People.cs
+++ Utils/People.cs
@@ -3,7 +3,7 @@
     public static class People
     {
         public const long Kana = 236852510688542720;
-        public const long Sumi = 90703004650467328;
+        public const long Sumi = 252142384303833088;
         public const long Fluffy = 240899183555379202;
     }
 }
