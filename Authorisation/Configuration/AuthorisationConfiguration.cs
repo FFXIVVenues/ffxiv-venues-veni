c764a721414b18fc92ed1af672970780778d791d
Kana Ki <kana@ksharp.net>
2023-06-03 21:46:24 +0100

    Provide defaults for all authorisation configuration properties

--- Authorisation/Configuration/AuthorisationConfiguration.cs
+++ Authorisation/Configuration/AuthorisationConfiguration.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace FFXIVVenues.Veni.Authorisation.Configuration;
 
 public class AuthorisationConfiguration
 {
-    public ulong[] Master { get; set; }
-    public PermissionSet[] PermissionSets { get; set; }
-    public Permission[] ManagerPermissions { get; set; }
+    public ulong[] Master { get; set; } = Array.Empty<ulong>();
+    public PermissionSet[] PermissionSets { get; set; } = Array.Empty<PermissionSet>();
+    public Permission[] ManagerPermissions { get; set; } = Array.Empty<Permission>();
 }
