8006b096bda556e992f5e3b22e83cee47b06e6e5
Kana Ki <kana@ksharp.net>
2023-06-14 01:44:22 +0100

    Add retry and do nothing to Audit actions

--- VenueAuditing/IVenueAuditService.cs
+++ VenueAuditing/IVenueAuditService.cs
@@ -7,6 +7,7 @@ public interface IVenueAuditService
 {
     VenueAudit CreateAuditFor(Venue venue, string roundId, ulong requestedIn, ulong requestedBy);
     VenueAudit CreateAuditFor(Venue venue, VenueAuditRecord record);
+    Task<VenueAuditRecord> GetAudit(string auditId);
     Task<VenueAuditRecord> GetLatestRecordFor(Venue venue);
     Task<VenueAuditRecord> GetLatestRecordFor(string venueId);
     Task UpdateAuditStatus(Venue venue, ulong actingUserId, VenueAuditStatus status);
