4996ccb8562dc49a0af91dab5e47b38b4ed494e2
Kana Ki <kana@ksharp.net>
2023-10-26 04:18:33 +0100

    Add Oceania!!

--- VenueControl/VenueAuthoring/PropertyEntrySessionStates/ScheduleEntry/TimeZoneEntryState.cs
+++ VenueControl/VenueAuthoring/PropertyEntrySessionStates/ScheduleEntry/TimeZoneEntryState.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -6,47 +7,49 @@ using FFXIVVenues.Veni.Infrastructure.Context;
 using FFXIVVenues.Veni.Infrastructure.Context.SessionHandling;
 using FFXIVVenues.Veni.Utils;
 
-namespace FFXIVVenues.Veni.VenueControl.VenueAuthoring.PropertyEntrySessionStates.ScheduleEntry
+namespace FFXIVVenues.Veni.VenueControl.VenueAuthoring.PropertyEntrySessionStates.ScheduleEntry;
+
+class TimeZoneEntrySessionState : ISessionState
 {
-    class TimeZoneEntrySessionState : ISessionState
+
+    private static string[] _messages = new[]
     {
+        "What **time zone** would you like to give opening times in?",
+        "What **time zone** would the venues opening times be in?"
+    };
 
-        private static string[] _messages = new[]
-        {
-            "What **time zone** would you like to give opening times in?",
-            "What **time zone** would the venues opening times be in?"
-        };
+    private static Dictionary<string, string> _timezones = new()
+    {
+        { "America/New_York", "Eastern Standard Time (EST)" },
+        { "America/Chicago", "Central Standard Time (CST)" },
+        { "America/Denver", "Mountain Standard Time (MST)" },
+        { "America/Los_Angeles", "Pacific Standard Time (PST)" },
+        { "America/Halifax", "Atlantic Standard Time (AST)" },
+        { "Europe/Budapest", "Central European Time (CEST)" },
+        { "Europe/Chisinau", "Eastern European Time (EEST)" },
+        { "Europe/London", "GMT Time (GMT)" },
+        { "UTC", "Server Time (UTC)" },
+        { "Australia/Perth", "Australian Western Time (AWST)" },
+        { "Australia/Adelaide", "Australian Central Time (ACST)" },
+        { "Australia/Sydney", "Australian Eastern Time (AEST)" }
+    };
 
-        private static Dictionary<string, string> _timezones = new()
-        {
-            { "America/New_York", "Eastern Standard Time (EST)" },
-            { "America/Chicago", "Central Standard Time (CST)" },
-            { "America/Denver", "Mountain Standard Time (MST)" },
-            { "America/Los_Angeles", "Pacific Standard Time (PST)" },
-            { "America/Halifax", "Atlantic Standard Time (AST)" },
-            { "Europe/Budapest", "Central European Time (CEST)" },
-            { "Europe/Chisinau", "Eastern European Time (EEST)" },
-            { "Europe/London", "GMT Time (GMT)" },
-            { "UTC", "Server Time (UTC)" }
-        };
-
-        public Task Enter(VeniInteractionContext c)
-        {
-            var component = new ComponentBuilder();
-            var timezoneOptions = _timezones.Select(dc => new SelectMenuOptionBuilder(dc.Value, dc.Key)).ToList();
-            var selectMenu = new SelectMenuBuilder();
-            selectMenu.WithOptions(timezoneOptions);
-            selectMenu.WithCustomId(c.Session.RegisterComponentHandler(Handle, ComponentPersistence.ClearRow));
+    public Task Enter(VeniInteractionContext c)
+    {
+        var component = new ComponentBuilder();
+        var timezoneOptions = _timezones.Select(dc => new SelectMenuOptionBuilder(dc.Value, dc.Key)).ToList();
+        var selectMenu = new SelectMenuBuilder();
+        selectMenu.WithOptions(timezoneOptions);
+        selectMenu.WithCustomId(c.Session.RegisterComponentHandler(Handle, ComponentPersistence.ClearRow));
             
-            return c.Interaction.RespondAsync($"{MessageRepository.ConfirmMessage.PickRandom()} { _messages.PickRandom()}",
-                                  component.WithSelectMenu(selectMenu).WithBackButton(c).Build());
-        }
+        return c.Interaction.RespondAsync($"{MessageRepository.ConfirmMessage.PickRandom()} { _messages.PickRandom()}",
+            component.WithSelectMenu(selectMenu).WithBackButton(c).Build());
+    }
 
-        public Task Handle(MessageComponentVeniInteractionContext c)
-        {
-            var selectedTimezone = c.Interaction.Data.Values.Single();
-            c.Session.SetItem("timeZoneId", selectedTimezone);
-            return c.Session.MoveStateAsync<DaysEntrySessionState>(c);
-        }
+    private Task Handle(MessageComponentVeniInteractionContext c)
+    {
+        var selectedTimezone = c.Interaction.Data.Values.Single();
+        c.Session.SetItem("timeZoneId", selectedTimezone);
+        return c.Session.MoveStateAsync<DaysEntrySessionState>(c);
     }
 }
