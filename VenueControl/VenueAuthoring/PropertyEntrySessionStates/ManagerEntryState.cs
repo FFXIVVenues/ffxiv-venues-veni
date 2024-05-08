53e6a2589e535e3ba33b74538b776e4e62695377
Andy James <andrewj@ksharp.net>
2024-05-08 19:00:22 +0100

    Fix Veni trying to add herself as a manager, naughty!

--- VenueControl/VenueAuthoring/PropertyEntrySessionStates/ManagerEntryState.cs
+++ VenueControl/VenueAuthoring/PropertyEntrySessionStates/ManagerEntryState.cs
@@ -8,45 +8,38 @@ using FFXIVVenues.Veni.Infrastructure.Context.SessionHandling;
 using FFXIVVenues.Veni.Utils;
 using Newtonsoft.Json.Linq;
 
-namespace FFXIVVenues.Veni.VenueControl.VenueAuthoring.PropertyEntrySessionStates
+namespace FFXIVVenues.Veni.VenueControl.VenueAuthoring.PropertyEntrySessionStates;
+
+class ManagerEntrySessionState(IAuthorizer authorizer) : ISessionState
 {
-    class ManagerEntrySessionState : ISessionState
+    public Task Enter(VeniInteractionContext c)
     {
-        private readonly IAuthorizer _authorizer;
+        var venue = c.Session.GetVenue();
 
-        public ManagerEntrySessionState(IAuthorizer authorizer)
-        {
-            this._authorizer = authorizer;
-        }
-
-        public Task Enter(VeniInteractionContext c)
-        {
-            var venue = c.Session.GetVenue();
-
-            if (!this._authorizer.Authorize(c.Interaction.User.Id, Permission.EditManagers, venue).Authorized)
-                return c.Session.MoveStateAsync<ConfirmVenueSessionState>(c);
-
-            c.Session.RegisterMessageHandler(this.OnMessageReceived);
-            return c.Interaction.RespondAsync("Who is/are the manager(s)? :heart:",
-                                               new ComponentBuilder()
-                                               .WithBackButton(c)
-                                               .WithSkipButton<ConfirmVenueSessionState, ConfirmVenueSessionState>(c)
-                                               .Build());
-        }
-
-        public Task OnMessageReceived(MessageVeniInteractionContext c)
-        {
-            var venue = c.Session.GetVenue();
-
-            var regex = new Regex("[0-9]{17,}");
-            var discordIds = regex.Matches(c.Interaction.Content).Select(m => m.Value);
-
-            if (discordIds == null || !discordIds.Any())
-                return c.Interaction.Channel.SendMessageAsync(MessageRepository.DontUnderstandResponses.PickRandom());
-
-            venue.Managers = discordIds.ToList();
+        if (!authorizer.Authorize(c.Interaction.User.Id, Permission.EditManagers, venue).Authorized)
             return c.Session.MoveStateAsync<ConfirmVenueSessionState>(c);
-        }
 
+        c.Session.RegisterMessageHandler(this.OnMessageReceived);
+        return c.Interaction.RespondAsync("Who is/are the manager(s)? :heart:",
+            new ComponentBuilder()
+                .WithBackButton(c)
+                .WithSkipButton<ConfirmVenueSessionState, ConfirmVenueSessionState>(c)
+                .Build());
     }
+
+    public Task OnMessageReceived(MessageVeniInteractionContext c)
+    {
+        var venue = c.Session.GetVenue();
+
+        var regex = new Regex("[0-9]{17,}");
+        var input = c.Interaction.Content.StripMentions(c.Client.CurrentUser.Id);
+        var discordIds = regex.Matches(input).Select(m => m.Value).ToList();
+
+        if (discordIds is not { Count: > 0})
+            return c.Interaction.Channel.SendMessageAsync(MessageRepository.DontUnderstandResponses.PickRandom());
+
+        venue.Managers = discordIds;
+        return c.Session.MoveStateAsync<ConfirmVenueSessionState>(c);
+    }
+
 }
