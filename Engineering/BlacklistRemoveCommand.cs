98f28a4a203d29bde7b37b3448a47a85830d7a56
Andy James <andrewj@ksharp.net>
2024-11-25 20:14:06 +0000

    Add discordid command option

--- Engineering/BlacklistRemoveCommand.cs
+++ Engineering/BlacklistRemoveCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading.Tasks;
+using Discord;
 using FFXIVVenues.Veni.Authorisation;
 using FFXIVVenues.Veni.Authorisation.Blacklist;
 using FFXIVVenues.Veni.Infrastructure.Commands;
@@ -10,24 +11,16 @@ using FFXIVVenues.Veni.Utils;
 namespace FFXIVVenues.Veni.Engineering;
 
 [DiscordCommand("root blacklist remove", "Remove a discord guild or user from the blacklist.")]
-public class BlacklistRemoveCommand : ICommandHandler
+[DiscordCommandOption("discordid", "Discord ID of guild/user", ApplicationCommandOptionType.String)]
+public class BlacklistRemoveCommand(IRepository db, IAuthorizer authorizer) : ICommandHandler
 {
-    private readonly IRepository _db;
-    private readonly IAuthorizer _authorizer;
-
-    public BlacklistRemoveCommand(IRepository db, IAuthorizer authorizer)
-    {
-        this._db = db;
-        this._authorizer = authorizer;
-    }
-
     public async Task HandleAsync(SlashCommandVeniInteractionContext slashCommand)
     {
-        if (!_authorizer.Authorize(slashCommand.Interaction.User.Id, Permission.Blacklist).Authorized)
+        if (!authorizer.Authorize(slashCommand.Interaction.User.Id, Permission.Blacklist).Authorized)
             return;
 
         await slashCommand.Interaction.DeferAsync();
-        await _db.DeleteAsync<BlacklistEntry>(id: slashCommand.GetStringArg("discordid"));
+        await db.DeleteAsync<BlacklistEntry>(id: slashCommand.GetStringArg("discordid"));
         await slashCommand.Interaction.FollowupAsync("Discord ID either was removed or wasnt on the blacklist 😊");
     }
 }
