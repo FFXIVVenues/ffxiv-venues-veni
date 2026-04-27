e8920b16e4ecb000f57ca48949a4e87d0d1a23f4
Kana Ki <kana@ffxivvenues.com>
2026-04-27 01:07:38 +0100

    Add descriptions to game and feature tags for improved clarity

--- VenueControl/VenueAuthoring/PropertyEntrySessionStates/TagsEntry/GamesEntryState.cs
+++ VenueControl/VenueAuthoring/PropertyEntrySessionStates/TagsEntry/GamesEntryState.cs
@@ -13,15 +13,15 @@ class GamesEntrySessionState : ISessionState
 {
     private Venue _venue;
 
-    private static List<(string Label, string Value)> _availableGames = new()
+    private static List<(string Label, string Description, string Value)> _availableGames = new()
     {
-        ("Triple triad", "Triple triad"),
-        ("Truth or Dare", "Truth or Dare"),
-        ("Blackjack", "Blackjack"),
-        ("Deathroll", "Deathroll"),
-        ("Texas Holdem", "Texas Holdem"),
-        ("Bingo", "Bingo"),
-        ("Roulette", "Roulette")
+        ("Triple triad", "The venue offers individual or competitive games of Triple Triad via the game interface itself.", "Triple triad"),
+        ("Truth or dare", "The venue offers a game of Truth or Dare for whomever feels like joining. Sometimes in private alliance, sometimes venue wide.", "Truth or dare"),
+        ("Blackjack", "The venue holds a table for players who wish to gamble gil in a game of 21.", "Blackjack"),
+        ("Deathroll", "The venue offers individual or competitive game of Deathroll usually with prizes involved.", "Deathroll"),
+        ("Texas holdem", "The venue holds a table for players who wish to gamble gil in a game of Texas holdem.", "Texas holdem"),
+        ("Bingo", "The venue holds a venue wide bingo game, usually with prizes involved.", "Bingo"),
+        ("Roulette", "The venue holds a table for players who wish to gamble gil in a game of Roulette.", "Roulette")
     };
 
     public Task Enter(VeniInteractionContext c)
@@ -37,8 +37,8 @@ class GamesEntrySessionState : ISessionState
         var selectComponent = new SelectMenuBuilder()
             .WithCustomId(c.Session.RegisterComponentHandler(OnComplete, ComponentPersistence.ClearRow))
             .WithMaxValues(_availableGames.Count);
-        foreach (var (label, value) in _availableGames)
-            selectComponent.AddOption(label, value, isDefault: this._venue.Tags.Contains(value));
+        foreach (var (label, description, value) in _availableGames)
+            selectComponent.AddOption(label, value, isDefault: this._venue.Tags.Contains(value), description: description);
 
         return new ComponentBuilder()
             .WithSelectMenu(selectComponent)
